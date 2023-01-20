1f935a74d3676d418d8e8dd1591efd953dbe9350
Asherbeck10 <asher.bec@gmail.com>
2023-01-20 14:51:16 +0000

    lister added

--- starter/script.js
+++ starter/script.js
@@ -1,2 +1,21 @@
+let $buttonListEl=$('.input-group')
+
+//current day at the top of the calender
 let today = moment();
-$("#currentDay").text(today.format("D MMM YYYY"));
+$('#currentDay').text(today.format('D MMM YYYY'));
+//Delegate event listener to the parent element, 
+
+
+
+
+
+$buttonListEl.on('click',function(event) {
+    
+    let $submitText=$(event.target).siblings('input').val()
+
+
+    console.log($submitText)
+})
+
+
+
